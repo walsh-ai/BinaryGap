42befd9ef8eefcae2fcc739b773daaf00b7259b9
csnirs <walsh.jthomas@outlook.com>
2023-08-29 18:05:31 +0100

    Third commit, added comments in method.

--- Program.cs
+++ Program.cs
@@ -1,13 +1,13 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
 
-// Test the solution 
-int result = LargestBinaryGap(1041); 
-Console.WriteLine($"Largest binary gap in 1041: {result}"); 
-Console.WriteLine($"Test with small number (2): {LargestBinaryGap(2)}"); 
-Console.WriteLine($"Test with (5): {LargestBinaryGap(5)}"); 
-Console.WriteLine($"Test with (-10): {LargestBinaryGap(-10)}"); 
-Console.WriteLine($"Test with (430034): {LargestBinaryGap(430034)}"); // Should be 3 
+// Test the solution for base case and valid integers 
+Console.WriteLine($"Largest binary gap in 1041 (5 Expected): {LargestBinaryGap(1041)}"); 
+Console.WriteLine($"Test with int 430034 (3 Expected): {LargestBinaryGap(430034)}"); 
+Console.WriteLine($"Test with int 5 (1 Expected): {LargestBinaryGap(5)}"); 
+Console.WriteLine($"Test base case less than 3 digits in binary int 2 (0 Expected): {LargestBinaryGap(2)}"); 
+Console.WriteLine($"Test base case negative integer -10 (Expected 0): {LargestBinaryGap(-10)}"); 
+ 
 
 public static partial class Program {
     /// <summary>
@@ -24,13 +24,23 @@ public static partial class Program {
         if ((x < 0) || binaryString.Length < 3) 
             return 0; 
         
-        // The algorithm 
+        /* 
+        * My algorithmic solution 
+        */
+
+        // Maintain the largest zero-gap counted and
+        // the current gap being counted 
         int largestGap = 0; 
         int currentGap = 0; 
 
+        // Iterate the digits in the binary string right -> left 
         for (int i = 0; i < binaryString.Length; i++) {
             bool digit = (binaryString[i] == '1'); 
             if (digit) {
+                // End count of zeros, set largestGap to currentGap if it is greater 
+                // Reset current gap to zero ready for next count 
+                // largestGap is unchanged when counting successive 1's 
+                //          as currentGap will be zero 
                 largestGap = Math.Max(largestGap, currentGap); 
                 currentGap = 0; 
             }
